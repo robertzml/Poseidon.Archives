d782e791bf84428b51ee8464bdd704ae5d5e0282
robertzml <robertzml@126.com>
2017-12-04 17:08:50 +0800

    Add read file.
    Signed-off-by: robertzml <robertzml@126.com>

--- Poseidon.Archives.Core/BL/AttachmentBusiness.cs
+++ Poseidon.Archives.Core/BL/AttachmentBusiness.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 namespace Poseidon.Archives.Core.BL
 {
     using Poseidon.Base.Framework;
+    using Poseidon.Base.System;
     using Poseidon.Archives.Core.DL;
     using Poseidon.Archives.Core.IDAL;
 
@@ -34,6 +36,27 @@ namespace Poseidon.Archives.Core.BL
         {
             var dal = this.baseDal as IAttachmentRepository;
             return dal.GetFolders();
+        }
+
+        /// <summary>
+        /// 读取附件
+        /// </summary>
+        /// <param name="root">根目录</param>
+        /// <param name="id">ID</param>
+        /// <returns></returns>
+        public FileStream ReadFile(string root, string id)
+        {
+            var attachment = this.baseDal.FindById(id);
+            if (attachment == null)
+                throw new PoseidonException(ErrorCode.ObjectNotFound);
+
+            string path =Path.Combine(root, attachment.Folder, attachment.FileName);
+
+            if (!File.Exists(path)) //文件已删除
+                throw new PoseidonException(ErrorCode.ObjectDeleted);
+
+            FileStream fs = new FileStream(path, FileMode.Open);
+            return fs;            
         }
         #endregion //Method
     }
